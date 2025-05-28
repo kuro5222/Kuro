34b371096df0c61ae595f6ecf4c2e81b8d06db45
kuro5222 <kienharljeckg@gmail.com>
2025-05-28 14:56:42 +0800

    Create 3 in 1.lua

---
+++ 3 in 1.lua
@@ -0,0 +1,6 @@
+--Notif
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/main/Notif%20player.lua"))()
+--Notif on chat
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/main/Joined%20Left.lua"))()
+--Notif died
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/refs/heads/main/Dead%20notify.lua"))()
