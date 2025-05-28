1efe8fbd4d4848a1c393d2e9f17dfc24f0eaf160
kuro5222 <kienharljeckg@gmail.com>
2025-05-28 15:00:55 +0800

    Update 3 in 1.lua

--- 3 in 1.lua
+++ 3 in 1.lua
@@ -3,4 +3,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/main/No
 --Notif on chat
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/main/Joined%20Left.lua"))()
 --Notif died
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/refs/heads/main/Dead%20notify.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/main/Dead%20notify.lua"))()
