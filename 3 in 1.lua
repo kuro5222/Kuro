f619edc4b05f4730062b1492836b5e8ec11e7a16
kuro5222 <kienharljeckg@gmail.com>
2025-06-02 20:19:57 +0800

    Update 3 in 1.lua

--- 3 in 1.lua
+++ 3 in 1.lua
@@ -1,36 +1,14 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Notif Joined/Leave",
-    Text = "Successfully executed",
-    Duration = 2,
-})
-wait(0.1)
 --Notif
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/main/Notif%20player.lua"))()
 
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Notif on chat",
-    Text = "Successfully executed",
-    Duration = 2,
-})
-wait(0.1)
+wait()
 --Notif on chat
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/main/Joined%20Left.lua"))()
 
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Notif Died",
-    Text = "Successfully executed",
-    Duration = 2,
-})
-wait(0.1)
-
+wait()
 --Notif died
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/main/Dead%20notify.lua"))()
 
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Shiftlock",
-    Text = "Successfully executed",
-    Duration = 2,
-})
-
+wait()
 --shiftlock
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kuro5222/Kuro/main/ShiftLock.lua"))()
